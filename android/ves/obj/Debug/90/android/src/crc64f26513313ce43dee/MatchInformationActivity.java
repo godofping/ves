d43d228002b90b798a7145b4351ab9176b96e837
D Murphy <rexmicrosoft@yahoo.com.ph>
2020-06-29 09:37:36 +0800

    a

--- android/ves/obj/Debug/90/android/src/crc64f26513313ce43dee/MatchInformationActivity.java
+++ android/ves/obj/Debug/90/android/src/crc64f26513313ce43dee/MatchInformationActivity.java
@@ -5,7 +5,8 @@ public class MatchInformationActivity
 	extends android.app.Activity
 	implements
 		mono.android.IGCUserPeer,
-		android.app.DatePickerDialog.OnDateSetListener
+		android.app.DatePickerDialog.OnDateSetListener,
+		android.app.TimePickerDialog.OnTimeSetListener
 {
 /** @hide */
 	public static final String __md_methods;
@@ -14,6 +15,7 @@ public class MatchInformationActivity
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
 			"n_onCreateDialog:(I)Landroid/app/Dialog;:GetOnCreateDialog_IHandler\n" +
 			"n_onDateSet:(Landroid/widget/DatePicker;III)V:GetOnDateSet_Landroid_widget_DatePicker_IIIHandler:Android.App.DatePickerDialog/IOnDateSetListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onTimeSet:(Landroid/widget/TimePicker;II)V:GetOnTimeSet_Landroid_widget_TimePicker_IIHandler:Android.App.TimePickerDialog/IOnTimeSetListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
 			"";
 		mono.android.Runtime.register ("ves.MatchInformationActivity, ves", MatchInformationActivity.class, __md_methods);
 	}
@@ -49,6 +51,14 @@ public class MatchInformationActivity
 	}
 
 	private native void n_onDateSet (android.widget.DatePicker p0, int p1, int p2, int p3);
+
+
+	public void onTimeSet (android.widget.TimePicker p0, int p1, int p2)
+	{
+		n_onTimeSet (p0, p1, p2);
+	}
+
+	private native void n_onTimeSet (android.widget.TimePicker p0, int p1, int p2);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
